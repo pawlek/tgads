e828773d29ec8093a0f0f7d0d6fabba878f80dc2
Pawlek <pavlo.voronyuk@gmail.com>
2023-10-16 13:17:28 +0200

    upd

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,14 @@
 let tg = window.Telegram.WebApp;
 
 tg.expand();
+
+const heightOutput = document.querySelector("#height");
+const widthOutput = document.querySelector("#width");
+
+function updateSize() {
+  heightOutput.textContent = window.innerHeight;
+  widthOutput.textContent = window.innerWidth;
+}
+
+updateSize();
+window.addEventListener("resize", updateSize);
