c57d3702bf4cf0c1a96ddf85d627c7618bdc2502
Pawlek <pavlo.voronyuk@gmail.com>
2023-10-16 14:18:22 +0200

    upd

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,6 +2,9 @@ let tg = window.Telegram.WebApp;
 
 // tg.expand();
 
+tg.MainButton.show();
+tg.MainButton.disable();
+
 const heightOutput = document.querySelector("#height");
 const widthOutput = document.querySelector("#width");
 
@@ -21,11 +24,12 @@ tg.MainButton.textColor = "#F55353"; //изменяем цвет текста к
 tg.MainButton.color = "#143F6B"; //изменяем цвет бэкграунда кнопки
 tg.MainButton.setParams({ color: "#143F6B" }); //так изменяются все параметры
 
-btn.addEventListener('click', function(){ //вешаем событие на нажатие html-кнопки
-	if (tg.MainButton.isVisible){ //если кнопка показана 
-		tg.MainButton.hide() //скрываем кнопку 
+btn.addEventListener("click", function () {
+	//вешаем событие на нажатие html-кнопки
+	if (tg.MainButton.isVisible && !tg.MainButton.isVisible) {
+		tg.MainButton.enable();
+	} else {
+		//иначе
+		tg.MainButton.show(); //показываем
 	}
-  else{ //иначе
-  	tg.MainButton.show() //показываем 
-  }
 });
