c9c92b4ceee23342cabe143e7a55971491951950
Pawlek <pavlo.voronyuk@gmail.com>
2023-10-16 13:59:32 +0200

    upd

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,7 +9,7 @@ heightOutput.textContent = window.innerHeight;
 widthOutput.textContent = window.innerWidth;
 
 let item = document.querySelectorAll(".item");
-let margin = 16;
+let margin = 32;
 for (var i = 0; i < item.length; i++) {
 	item[i].style.width = (window.innerWidth - margin * 3) / 2 + "px";
 	item[i].style.height = (window.innerWidth - margin * 3) / 2 + "px";
