78b7b0891f6a22bb06d033a5bb6ebab81c5f7868
Pawlek <pavlo.voronyuk@gmail.com>
2023-10-31 23:36:10 +0100

    upd

--- assets/js/main.js
+++ assets/js/main.js
@@ -21,13 +21,13 @@ for (var i = 0; i < item.length; i++) {
 let usercard = document.getElementById("user"); //Используем getElementById, чтобы получить карточку пользователя
 
 let profileName = document.createElement("p"); //При помощи document.createElement делаем абзац – <p> </p>
-profileName.textContent = `${telegramAppChatLabs.initDataUnsafe.user.first_name}
-   ${telegramAppChatLabs.initDataUnsafe.user.last_name}
-   ${telegramAppChatLabs.initDataUnsafe.user.username} (${telegramAppChatLabs.initDataUnsafe.user.language_code})`;
+profileName.textContent = `${tg.initDataUnsafe.user.first_name}
+   ${tg.initDataUnsafe.user.last_name}
+   ${tg.initDataUnsafe.user.username} (${tg.initDataUnsafe.user.language_code})`;
 //В созданном параграфе будет Имя пользователя, его Фамилия, username, а также код языка
 usercard.appendChild(profileName); //Используем appendChild, чтобы добавить узел в конец списка дочерних элементов
 let userid = document.createElement("p"); // Используем document.createElement для создания еще одного абзаца
-userid.textContent = `${telegramAppChatLabs.initDataUnsafe.user.id}`; // Отображаем id пользователя
+userid.textContent = `${tg.initDataUnsafe.user.id}`; // Отображаем id пользователя
 usercard.appendChild(userid);
 
 tg.MainButton.text = "Select any package"; //изменяем текст кнопки
